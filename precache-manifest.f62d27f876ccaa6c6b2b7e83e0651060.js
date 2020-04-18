c1827476cdfbaec481c0a668f61b516a0778faf8
IlyaGAnyVision <ilyag@anyvision.co>
2020-04-18 19:26:34 +0300

    Updates

---
+++ precache-manifest.f62d27f876ccaa6c6b2b7e83e0651060.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97e836cf000f63e8401f96747cddba84",
+    "url": "/memory-game/index.html"
+  },
+  {
+    "revision": "d2dc41a0c4f1f9fbf46d",
+    "url": "/memory-game/static/css/main.7045cd8e.chunk.css"
+  },
+  {
+    "revision": "17bac5198aa2794c214b",
+    "url": "/memory-game/static/js/2.a6abfe68.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/memory-game/static/js/2.a6abfe68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2dc41a0c4f1f9fbf46d",
+    "url": "/memory-game/static/js/main.883ee0fe.chunk.js"
+  },
+  {
+    "revision": "80cdb062924655130cea",
+    "url": "/memory-game/static/js/runtime-main.e88f98c1.js"
+  }
+]);
